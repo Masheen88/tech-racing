e0ccf44506248ff3f54b3f60598429e75b1b0abb
Masheen88 <mattdrummerguy@gmail.com>
2024-03-01 15:16:59 -0600

    Update server to track car position and rotation

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const allowedOrigins = [
 
 const io = new Server(server, {
   cors: {
-    origin: allowedOrigins,
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -44,15 +44,55 @@ let users = []; // Track connected users
 io.on("connection", (socket) => {
   // console.clear();
   console.log("a user connected with id:", socket.id);
-  users.push(socket.id); // Add new user to list
+  users.push({
+    socketId: socket.id,
+    carPosition: { x: 0, y: 0 },
+    carRotation: 0,
+    speed: 0,
+    timestamp: null,
+  }); // Add new user to list
   console.log("users", users);
   // Broadcast updated user list to all clients
   io.emit("users", users);
 
   socket.on("disconnect", () => {
     console.log("user disconnected", socket.id);
-    users = users.filter((user) => user !== socket.id); // Remove disconnected user
+    // Correctly filter out the disconnected user
+    users = users.filter((user) => user.socketId !== socket.id);
     io.emit("users", users); // Update all clients
+  });
+
+  socket.on("updateCarPosition", (userId, position, rotation, speed) => {
+    // console.log(
+    //   "updateCarPosition",
+    //   "user ID:",
+    //   userId,
+    //   "position DATA:",
+    //   position
+    // );
+
+    // console.log("car rotation data:", rotation.current);
+    // Find the user in the users array and update their car position, this should alter the original users data
+    users = users.map((user) => {
+      if (user.socketId === userId) {
+        // console.log("found user", user.socketId, "updating position");
+
+        user.carPosition = position;
+        user.carRotation = rotation.current;
+        user.speed = speed;
+      }
+      return user;
+    });
+    // console.log("users", users);
+
+    //REVIEW
+    io.emit("carPositionUpdate", {
+      carId: userId,
+      position,
+      rotation,
+      speed,
+      timestamp: Date.now(),
+    });
   });
 });
 
